010e511922b87ce006720a383ec8944dd2851702
Jon Harrison <jonathan_harrison@hotmail.com>
2023-01-20 12:26:00 +0000

    update markdown to generate readme layout

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,42 @@
+import { licenses } from './licenses.js';
+
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+  ${licenses.makeBadge(data.license)}
+  
+  ## Description
+  ${data.description}
 
-`;
+  ## Table of Contents
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [License](#license)
+  4. [Contributing](#contributing)
+  5. [Tests](#tests)
+  6. [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  This project is licensed under ${data.license}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  * GitHub profile : [${data.username}](https://github.com/${data.username})
+  * Email : ${data.email}
+
+  `;
 }
 
-export { generateMarkdown };
+export { generateMarkdown, licenses };
