b73915834894aaaf23f4def093b118239aac7021
Jon Harrison <jonathan_harrison@hotmail.com>
2023-01-20 12:27:40 +0000

    add questions

--- index.js
+++ index.js
@@ -6,6 +6,52 @@ import util from 'util';
 
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is the project title?"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Please provide a description of the project"
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: "Please provide the installation instructions for the project"
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "Please provide usage information"
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: "Please provide guidelines on how to contribute to the project"
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: "Please provide test instructions"
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license for the application',
+        choices: licenses.getChoices()
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: "Please enter your GitHub username"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Please enter your contact email address"
+    }
 
 ];
 
