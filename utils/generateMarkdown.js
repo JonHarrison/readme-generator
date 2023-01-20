0af7ed4384cdb6ba98eeaba1902aedfd30614669
Jon Harrison <jonathan_harrison@hotmail.com>
2023-01-20 19:54:15 +0000

    add function to dynamically generate table of contents based on which headings have information entered

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,30 @@
 import { licenses } from './licenses.js';
 
+const generateToC = (data) => {
+  // destructure data object
+  const { installation, usage, license, contributing, tests, username, email } = data;
+  // define sections on table of contents
+  const ToC = [
+    { section: installation, content: '[Installation](#installation)' },
+    { section: usage, content: '[Usage](#usage)' },
+    { section: license, content: '[License](#license)' },
+    { section: contributing, content: '[Contributing](#contributing)' },
+    { section: tests, content: '[Tests](#tests)' },
+    { section: username || email, content: '[Questions](#questions)' }
+  ];
+
+  let item = 1; // ToC starts with item 1
+  let output = '## Table of Contents\n';
+
+  for (let element of ToC) {
+    if (element.section) { // if this section has any content add ToC
+      output += `${item++}. ${element.content}\n`;
+    }
+  }
+
+  return output;
+}
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `
