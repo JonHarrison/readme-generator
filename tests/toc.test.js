0722b9f57938eb895039ba26f9cd73eee6f0c82b
Jon Harrison <jonathan_harrison@hotmail.com>
2023-01-23 13:52:15 +0000

    refactor tests for table of contents

--- tests/toc.test.js
+++ tests/toc.test.js
@@ -1,31 +1,28 @@
 import { generateToC } from '../utils/generateMarkdown.js';
 
-test('Table of contents', () => {
-    const tests = [
-        {
-            data : { installation: 'dummy text' },
-            result: '## Table of Contents\n1. [Installation](#installation)\n'
-        },
-        {
-            data : { installation: 'dummy text', usage: 'dummy text' },
-            result: '## Table of Contents\n1. [Installation](#installation)\n2. [Usage](#usage)\n'
-        },
-        {
-            data : { installation: 'dummy text', usage: 'dummy text', license: 'dummy text', contributing: 'dummy text', tests: 'dummy text', username: 'dummy text' },
-            result: '## Table of Contents\n1. [Installation](#installation)\n2. [Usage](#usage)\n3. [License](#license)\n4. [Contributing](#contributing)\n5. [Tests](#tests)\n6. [Questions](#questions)\n'
-        },
-        {
-            data : { usage: 'dummy text' },
-            result: '## Table of Contents\n1. [Usage](#usage)\n'
-        },
-        {
-            data : { email: 'dummy text' },
-            result: '## Table of Contents\n1. [Questions](#questions)\n'
-        },
+const tests = [
+    [
+        { installation: 'dummy text' },
+        '## Table of Contents\n1. [Installation](#installation)\n'
+    ],
+    [
+        { installation: 'dummy text', usage: 'dummy text' },
+        '## Table of Contents\n1. [Installation](#installation)\n2. [Usage](#usage)\n'
+    ],
+    [
+        { installation: 'dummy text', usage: 'dummy text', license: 'dummy text', contributing: 'dummy text', tests: 'dummy text', username: 'dummy text' },
+        '## Table of Contents\n1. [Installation](#installation)\n2. [Usage](#usage)\n3. [License](#license)\n4. [Contributing](#contributing)\n5. [Tests](#tests)\n6. [Questions](#questions)\n'
+    ],
+    [
+        { usage: 'dummy text' },
+        '## Table of Contents\n1. [Usage](#usage)\n'
+    ],
+    [
+        { email: 'dummy text' },
+        '## Table of Contents\n1. [Questions](#questions)\n'
     ]
-    for(let test of tests)
-    {
-        let toc = generateToC(test.data);
-        expect(toc).toBe(test.result);
-    }
+]
+
+test.each(tests)('Table of contents', (data, result) => {
+    expect(generateToC(data)).toBe(result);
 })
